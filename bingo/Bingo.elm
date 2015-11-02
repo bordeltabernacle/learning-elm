cec7d03183a6139256d8628fe7c8ff75dc9b971d
Rob Phoenix <robertphoenix@gmail.com>
2015-11-02 16:38:28 +0000

    calling functions

--- bingo/Bingo.elm
+++ bingo/Bingo.elm
@@ -1,6 +1,25 @@
 module Bingo where
 
 import Html
+import String
+
+{-
+
+'Twas in a shady Avenue,
+Where lofty Elms abound--
+And from a Tree
+There came to me
+A sad and solemn sound,
+That sometimes murmur'd overhead,
+And sometimes underground.
+
+Credit: Thomas Hood's poem "The Elm Tree"
+
+-}
 
 main =
-    Html.text "Hello, World!"
+--    Html.text (String.repeat 3 (String.toUpper "bingo!"))
+    "bingo!"
+        |> String.toUpper
+        |> String.repeat 3
+        |> Html.text
